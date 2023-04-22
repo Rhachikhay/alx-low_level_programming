faaf0d8a35411eb2e0431917e3393748968914f9
“Rhachikhay” <rachealkareem2@gmail.com>
2023-04-21 19:00:26 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints alpha in lower class
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+	for (lc = 'a'; lc <= 'z'; lc++);
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
