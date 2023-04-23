9b1534ec55ea78e48915c5d0c719352e80ae796f
“Rhachikhay” <rachealkareem2@gmail.com>
2023-04-23 09:09:26 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in lower and upper case
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char rh;
+
+	for (rh = 'a'; rh <= 'z'; rh++)
+	       putchar(rh);
+
+        for (rh = 'A'; rh <= 'Z'; rh++)
+	       putchar(rh);
+
+        putchar('\n');
+	
+	return (0);
+}	
