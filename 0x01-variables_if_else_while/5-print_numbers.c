271ffa14cac514f2e74f20608009df12d209104f
“Rhachikhay” <rachealkareem2@gmail.com>
2023-04-24 15:25:28 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int r;
+
+	if (r = 0; r < 10; r++)
+	{
+		printf("%d", r);
+	}
+	putchar("\n");
+	return (0);
+}
