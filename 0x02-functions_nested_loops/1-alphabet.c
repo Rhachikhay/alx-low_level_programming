ef39c5114af1f6ebe170c296661acb322d760869
“Rhachikhay” <rachealkareem2@gmail.com>
2023-04-25 18:15:08 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+		_putchar(al);
+
+	_putchar('\n');
+}
