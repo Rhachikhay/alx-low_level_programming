fb1eef235bf66dfccd06f75a17cadc4ec746897d
“Rhachikhay” <rachealkareem2@gmail.com>
2023-04-25 18:27:50 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alpha 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char al;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (al = 'a'; al <= 'z'; al++)
+			_putchar(al);
+		_putchar('\n');
+	}
+}
