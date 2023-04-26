f3581e3e31bc16f5790e94252d97620e46bc0d53
“Rhachikhay” <rachealkareem2@gmail.com>
2023-04-25 19:45:08 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,8 @@
 /**
  * print_sign - prints the sign of a number
  * @r: the number to be printed
- * Return: 1 if the number is greater than zero, 0 if the number is zero, -1 if the number is less than zero
+ * Return: 1 if the number is greater than zero,
+ * 0 if the number is zero, -1 if the number is less than zero
  */
 int print_sign(int r)
 {
